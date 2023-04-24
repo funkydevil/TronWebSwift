b623ba46a65f7bfe24c082ffd4572d07914db2c0
Kirill <kirill.pyulzyu@gmail.com>
2023-04-24 12:06:29 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,14 +24,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+//         .target(name: "secp256k1"),
         .target(
             name: "TronWebSwift",
             dependencies: [
                 .product(name: "SwiftProtobuf", package: "swift-protobuf"),
                 "CryptoSwift",
 //                 .product(name: "Secp256k1Swift", package: "Secp256k1Swift"),
-                .target(name: "secp256k1"),
-                .product(name: "BIP32Swift", package: "Secp256k1Swift"),
+//                 .product(name: "BIP32Swift", package: "Secp256k1Swift"),
                 "PromiseKit"
             ]
         ),
